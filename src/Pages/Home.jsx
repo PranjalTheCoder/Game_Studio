7a146ae8459fa8a16e48178506d7ef2d6d206677
Pranjal Tamrakar <154547923+PranjalTheCoder@users.noreply.github.com>
2024-11-04 17:35:14 +0530

    Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,8 +3,11 @@ import React from "react";
 
 function Home (){
     return (
-        <div> Home </div>
-    )
+        <div className='grid grid-cols-4'>
+        <div className='bg-red-600 h-full hidden md:block'> Genre </div>
+        <div className=' col-span-4 md:col-span-3 bg-blue-400'>Game List</div>
+        </div>
+    );
 }
 
 export default Home
