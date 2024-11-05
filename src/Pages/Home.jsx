26a2b610b46428c18b4ea44c8e4d2f5bc01ca9a6
Pranjal Tamrakar <154547923+PranjalTheCoder@users.noreply.github.com>
2024-11-05 14:06:50 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,33 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect, useState } from "react";
+import GenreList from "../Components/GenreList";
+import GlobalApi from "../Services/GlobalApi";
 
-function Home (){
+function Home() {
+    const [allGameList, setAllGameList] = useState([]);
+
+    useEffect(() => {
+        getAllGamesList();
+    }, []);
+
+    const getAllGamesList = () => {
+        GlobalApi.getAllGames().then((resp) => {
+            console.log(resp.data.results);
+            setAllGameList(resp.data.results);
+        });
+    };
+
     return (
-        <div className='grid grid-cols-4'>
-        <div className='bg-red-600 h-full hidden md:block'> Genre </div>
-        <div className=' col-span-4 md:col-span-3 bg-blue-400'>Game List</div>
+        <div className="grid grid-cols-4 px-8">
+            <div className="hidden md:block">
+                <GenreList />
+            </div>
+            <div className="col-span-4 md:col-span-3 bg-blue-400">
+                Game List
+                {/* Render games here if needed */}
+            </div>
         </div>
     );
 }
 
-export default Home
+export default Home;
